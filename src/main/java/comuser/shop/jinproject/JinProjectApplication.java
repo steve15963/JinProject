c3e8ffe337ca81a5c5aa2f5241dd04025d0d478f
steve15963 <steve15963@gmail.com>
2022-08-16 20:17:17 +0900

    Initial commit

---
+++ src/main/java/comuser/shop/jinproject/JinProjectApplication.java
@@ -0,0 +1,13 @@
+package comuser.shop.jinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinProjectApplication.class, args);
+    }
+
+}
